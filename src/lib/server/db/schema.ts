4853fc63b9a106b181567646492f53ee8afc5d34
Edmundo Ortega <ed.ortega@gmail.com>
2024-11-30 11:38:17 -0800

    Updated schema. Added shadcn

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,12 @@
 import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
 
+export const family = sqliteTable('family', {
+	id: integer('id').primaryKey(),
+	name: text('name').notNull()
+});
+
 export const user = sqliteTable('user', {
 	id: integer('id').primaryKey(),
-	age: integer('age')
+	age: integer('age'),
+	familyId: integer('family_id').references(() => family.id).notNull()
 });
